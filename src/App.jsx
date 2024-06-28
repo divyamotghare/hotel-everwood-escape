d349fdd07a37faea3b73e900ea220a4219b2cbdc
divyamotghare <divyamotghare9@gmail.com>
2024-06-28 20:37:27 +0530

    modifications

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,25 @@
+import styled from "styled-components";
+import GlobalStyle from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+import Heading from "./ui/Heading";
+
+const StyledApp = styled.div`
+  background-color: orangered;
+  padding: 20px;
+`;
+
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <>
+      <GlobalStyle />
+      <StyledApp>
+        <Heading>The Everwood Escape</Heading>
+        <Button onClick={() => alert("check out")}>check in</Button>
+        <Input type="number" placeholder="Number of guests" />
+      </StyledApp>
+    </>
+  );
 }
 
 export default App;
