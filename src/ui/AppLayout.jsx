31157f6d250cdebd6668451a93ecdfda30f1c46c
divyamotghare <divyamotghare9@gmail.com>
2024-07-27 14:01:47 +0530

    added styling

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -16,6 +16,14 @@ const StyledAppLayout = styled.div`
   height: 100vh;
 `;
 
+const Container = styled.div`
+  max-width: 120rem;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  gap: 3.2rem;
+`;
+
 function AppLayout() {
   return (
     <StyledAppLayout>
@@ -23,7 +31,9 @@ function AppLayout() {
       <Sidebar />
 
       <Main>
-        <Outlet />
+        <Container>
+          <Outlet />
+        </Container>
       </Main>
     </StyledAppLayout>
   );
