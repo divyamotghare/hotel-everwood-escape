1845bb4c60674757e4a6516a3c9475de983aa52a
divyamotghare <divyamotghare9@gmail.com>
2024-07-28 14:13:36 +0530

    exporting url

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://mjdtrbrmppzvcvqxvpib.supabase.co";
+export const supabaseUrl = "https://mjdtrbrmppzvcvqxvpib.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1qZHRyYnJtcHB6dmN2cXh2cGliIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTk1ODcxMjksImV4cCI6MjAzNTE2MzEyOX0.hA3I7ZA5xQB3EcEoqiZUCqZa1W9PKXnSC9dLAoMbaWk";
 const supabase = createClient(supabaseUrl, supabaseKey);
